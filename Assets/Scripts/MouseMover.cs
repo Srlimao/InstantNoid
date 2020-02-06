c11e209146b3c8e8f7bb4f20eb39cfd438f3e218
Srlimao <w.back93@gmail.com>
2020-02-05 21:57:25 -0300

    seila

---
+++ Assets/Scripts/MouseMover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MouseMover : MonoBehaviour
+{
+    Rigidbody2D rb = null;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.MovePosition(Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()));
+    }
+}
